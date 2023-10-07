afab9a0ffb3588cc89c35f868fa02ea77f1a51fa
Naohito-T <naohito.tanaka0523@gmail.com>
2023-10-07 17:10:39 +0900

    :recycle: Add: proxy

---
+++ src/design/behavioral/proxy/index.ts
@@ -0,0 +1,170 @@
+import * as chalk from 'chalk';
+import { prompt, Answers } from 'inquirer';
+import { BaseCommand } from '@/command/_base.command';
+import { DesignPatternInfo } from '@/design/design.interface';
+import { ILogger } from '@/logger';
+
+interface ProxyAnswer extends Answers {
+  outputs: ('help' | 'exec' | 'description' | 'flow-chart' | 'example-code')[];
+}
+
+const defaultQuestion = {
+  type: 'checkbox',
+  name: 'outputs', // keyの設定
+  default: 'help',
+};
+
+export class Proxy extends BaseCommand<ProxyAnswer> implements DesignPatternInfo {
+  public readonly question;
+
+  constructor(private readonly logger: ILogger) {
+    super();
+    this.question = this.buildQuestion({
+      ...defaultQuestion,
+      ...{
+        message: `-------------------------------\n  ${chalk.bold.blue(
+          `Proxyの項目から実行するパターンを選んでください。\n`,
+        )}`,
+        choices: [
+          { name: 'help' },
+          { name: 'exec' },
+          { name: 'description' },
+          { name: 'flow-chart' },
+          { name: 'example-code' },
+        ],
+      },
+    });
+  }
+
+  public run = async (): Promise<void> => {
+    const selectedOptions = await prompt(this.question);
+
+    this.logger.debug(`Composite answers: ${JSON.stringify(selectedOptions)}`);
+
+    const outputMsg: string[] = [];
+
+    selectedOptions.outputs.forEach((option) => {
+      if (option === 'help') {
+        console.log('helpが選択されました');
+        return; // 脱出するように
+      } else if (option === 'exec') {
+        console.log('execが選択されました');
+        this.exec();
+        return; // 脱出するように
+      } else if (option === 'description') {
+        outputMsg.push(this.description());
+      } else if (option === 'flow-chart') {
+        outputMsg.push(this.flowChart());
+      } else if (option === 'example-code') {
+        outputMsg.push(this.exampleCode());
+      }
+    });
+
+    outputMsg.forEach((msg) => console.log(msg));
+  };
+
+  public description = () => `${chalk.bold.bgGreen(`[description]`)}`;
+
+  /** @wip */
+  public flowChart = () => {
+    return ``;
+  };
+
+  public exampleCode = (): string => `
+${chalk.bold.bgGreen(`[example code]`)}
+// この例では、\`ProxySubject\`は\`RealSubject\`へのアクセスを代理しており、実際の要求の前後でアクセスのチェックやログの記録を行っています。
+// Subject Interface
+interface Subject {
+  request(): void;
+}
+
+// RealSubject
+class RealSubject implements Subject {
+  request(): void {
+    console.log('RealSubject: Handling request.');
+  }
+}
+
+// Proxy
+class ProxySubject implements Subject {
+  private realSubject: RealSubject;
+
+  constructor(realSubject: RealSubject) {
+    this.realSubject = realSubject;
+  }
+
+  // Subject interfaceで定義されているrequestを実装
+  request(): void {
+    // 実行前に処理を挟む。
+    if (this.checkAccess()) {
+      // Subject interfaceで実装した処理を行う。
+      this.realSubject.request();
+      // logを残す。
+      this.logAccess();
+    }
+  }
+
+  private checkAccess(): boolean {
+    console.log('Proxy: Checking access...');
+    return true;
+  }
+
+  private logAccess(): void {
+    console.log('Proxy: Logging the time of request.');
+  }
+}
+
+// Client code
+const realSubject = new RealSubject();
+console.log('Client: Executing the client code with a real subject:');
+realSubject.request();
+
+const proxy = new ProxySubject(realSubject);
+console.log('Client: Executing the client code with a proxy:');
+proxy.request();
+`;
+
+  public exec = (): void => {
+    interface Subject {
+      request(): void;
+    }
+
+    class RealSubject implements Subject {
+      request(): void {
+        console.log('RealSubject: Handling request.');
+      }
+    }
+
+    class ProxySubject implements Subject {
+      private realSubject: RealSubject;
+
+      constructor(realSubject: RealSubject) {
+        this.realSubject = realSubject;
+      }
+
+      request(): void {
+        if (this.checkAccess()) {
+          this.realSubject.request();
+          this.logAccess();
+        }
+      }
+
+      private checkAccess(): boolean {
+        console.log('Proxy: Checking access...');
+        return true;
+      }
+
+      private logAccess(): void {
+        console.log('Proxy: Logging the time of request.');
+      }
+    }
+
+    const realSubject = new RealSubject();
+    console.log('Client: Executing the client code with a real subject:');
+    realSubject.request();
+
+    const proxy = new ProxySubject(realSubject);
+    console.log('Client: Executing the client code with a proxy:');
+    proxy.request();
+  };
+}
